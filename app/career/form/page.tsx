f7154eaf70fa2227879f6b3ce5bcf9e9a09d320e
Kenzyyy29 <kenzyworld29@gmail.com>
2025-05-16 17:38:37 +0700

    fix:error-deployment 4

--- app/career/form/page.tsx
+++ app/career/form/page.tsx
@@ -1,5 +1,7 @@
 import CareerForm from "@/components/layouts/pages/Career/CareerForm";
 import {Metadata} from "next";
+import {Suspense} from "react";
+import {FaSpinner} from "react-icons/fa";
 
 export const metadata: Metadata = {
  title: "Lowongan Pekerjaan",
@@ -7,5 +9,14 @@ export const metadata: Metadata = {
 };
 
 export default function CareerFormPage() {
- return <CareerForm />;
+ return (
+  <Suspense
+   fallback={
+    <div className="h-[100dvh] flex items-center justify-center bg-white dark:bg-onyx1">
+     <FaSpinner className="text-3xl text-onyx1 dark:text-white animate-spin" />
+    </div>
+   }>
+   <CareerForm />
+  </Suspense>
+ );
 }
